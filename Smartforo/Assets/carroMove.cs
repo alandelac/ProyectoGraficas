38ef11f46d33cfc7008017a8c2e7ab8558c2ddf5
oscarsotoo1 <83682153+oscarsotoo1@users.noreply.github.com>
2022-08-25 13:38:38 -0700

    Creacion del modelo 3d, movimiento a la mitad
    oscar

--- Smartforo/Assets/carroMove.cs
+++ Smartforo/Assets/carroMove.cs
@@ -5,9 +5,40 @@ using UnityEngine;
 public class carroMove : MonoBehaviour
 {
 
-    int velocidadMax;
-    int aceleracion;
+    public Rigidbody rb;
+    public Transform car;
+    public float actualSpeed = 10.0f;
+    public GameObject[] checkpoints;
+    int counter = 0;
+    public float distance = 2.0f;
+    public Vector3 direction;
+
+    void Turn(float giro){
+        this.transform.Rotate(0,giro,0);
+    }
+
+    void moveForward(){
+        direction = Vector3.zero;
+        direction = checkpoints[counter].transform.position - transform.position;
+
+        if(direction.magnitude < distance){
+            if(counter < checkpoints.Length-1){
+                  counter++;
+            }
+            else{
+                counter = 0;
+            }
+        }  
+        direction = direction.normalized;
+        Vector3 dir = direction;
+ 
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direction.x * actualSpeed, direction.y * actualSpeed);
+    }
+
+
     WayPointContainer ruta;
+
+
 
 
     // Start is called before the first frame update
